80816d62da25b84f2fba69a9a4fb30ddf567219b
adelsonnelson <alexandre.reinato@gmail.com>
2024-08-23 10:40:59 -0300

    dsdfdfgd

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+from database import db
+from flask_migrate import Migrate
+from models import Usuario
+app.config['SECRET_KEY'] = ['2c1685c157fe2450fda443a0753bb69f']
+
+# drive://usuario:senha@servidor/banco_de_dados
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/bancoflask"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/aula')
+@app.route('/aula/<nome>')
+@app.route('/aula/<nome>/<curso>')
+@app.route('/aula/<nome>/<curso>/<int:ano>')
+def aula(nome = 'Joao', curso = 'Infomatica', ano = 1):
+    dados = {'nome':nome,'curso':curso,'ano':ano}
+    return render_template('aula.html', dados_curso = dados)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/dados', methods=['POST'])
+def dados():
+    flash('Dados enviados!')
+    dados = request.form
+    return render_template('dados.html', dados=dados)
+
+@app.route('/usuario')
+def usuario():
+    u = Usuario.query.all()
+    return render_template('usuario_lista.html', dados=u)
+
+if __name__ == '__main__':
+    app.run()
